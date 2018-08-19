2da53032c2dc178768cfc156723d697112527ab8
Dmitry Davletbaev <ddomgn@gmail.com>
2018-08-19 07:54:39 +0500

    Add application logic

--- src/main/java/ddomgn/rsscat/App.java
+++ src/main/java/ddomgn/rsscat/App.java
@@ -18,8 +18,49 @@
  */
 package ddomgn.rsscat;
 
+import static java.lang.System.out;
+
 public class App {
 
     public static void main(String[] args) {
+        Settings settings = new Settings();
+        settings.parseCmdOptions(args);
+        new App().doStuff(settings);
+    }
+
+    private void doStuff(Settings settings) {
+        if (settings.helpRequired()) {
+            printHelp();
+            return;
+        }
+        settings.feedUrls().stream().map(url -> {
+            try {
+                return new RssFeed(url).read();
+            } catch (Exception e) {
+                throw new Error(e);
+            }
+        }).forEach(channel -> {
+            out.println();
+            printLine(0, channel.title + ": " + channel.description);
+            channel.items.forEach(item -> {
+                printLine(1, item.title);
+                printLine(2, item.pubDate.toLocalDateTime().toString());
+                printLine(2, item.link);
+            });
+        });
+    }
+
+    private void printHelp() {
+        printLine(0, "rsscat: RSS reader with command line interface");
+        printLine(0, "Usage:");
+        printLine(1, "java -jar rsscat -h");
+        printLine(1, "java -jar rsscat URL1 [URL2 [...]]");
+        printLine(0, "-h, -help");
+        printLine(1, "Print help and exit");
+    }
+
+    private void printLine(int indent, String str) {
+        for (int i = 0; i < indent; i++) out.print("    ");
+        out.println(str);
     }
 }
